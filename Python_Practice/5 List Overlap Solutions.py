51f571ac3a2418f26caa8d013703f23320502080
Ivanbhub <bayingana12@gmail.com>
2021-04-14 16:36:09 -0500

    Create 5 List Overlap Solutions.py

---
+++ Python_Practice/5 List Overlap Solutions.py
@@ -0,0 +1,30 @@
+""" Take two lists, say for example these two:
+
+  a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+  b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+and write a program that returns a list that contains only the elements that are common between the lists (without duplicates). Make sure your program works on two lists of different sizes.
+
+Extras:
+
+Randomly generate two lists to test this
+Write this in one line of Python """
+
+
+
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+  
+List =[]
+
+#for x in a:
+ #   for y in b:
+  #      if x == y :
+   #         if x in List:
+    #            continue
+     #       List.append(x)
+            
+   #One liner          
+[(List.append(x))for x in a if x in b and x not in List ]
+
+print(List)
