4705ab60f8cdd470645ef7023eb1f9394ae53de8
Ivanbhub <bayingana12@gmail.com>
2021-04-21 22:17:01 -0500

    Update 11 prime numbers.py

--- Python_Practice/11 prime numbers.py
+++ Python_Practice/11 prime numbers.py
@@ -1,4 +1,7 @@
-"""Ask the user for a number and determine whether the number is prime or not. (For those who have forgotten, a prime number is a number that has no divisors.). You can (and should!) use your answer to Exercise 4 to help you. Take this opportunity to practice using functions, described below."""
+"""Ask the user for a number and determine whether the number 
+is prime or not. (For those who have forgotten, a prime number is a number that has no divisors.). 
+You can (and should!) use your answer to Exercise 4 to help you. 
+Take this opportunity to practice using functions, described below."""
 
 number = int(input('Type your number here --> '))
 
@@ -15,3 +18,6 @@ def  Prime_number (num):
         
         
 Prime_number (number)
+
+
+# didn't use my answer to Exercise 4
